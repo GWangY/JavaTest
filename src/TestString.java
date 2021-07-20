a9e7fff569855d724c4363f7be0e90edf834d5d6
GWangY <776024215@qq.com>
2021-07-20 15:55:13 +0800

    string;5.4

--- src/TestString.java
+++ src/TestString.java
@@ -6,20 +6,39 @@ public class TestString {
 		String str = "we are students";
 		int size = str.length(); 
 		System.out.println(size); //15
+		
 		//字符串查找 indexOf()
 		String str1="we are students";
 		int size1 = str.indexOf("a"); //输出结果是str1.length()-1
 		System.out.println(size1); // 3
+		
 		//str.lastIndexOf() 用于返回指定字符串最后一次出现的索引位置
 		String str2="we are students";
 		int size2=str.lastIndexOf("e");
 		System.out.println(size2); //11
+		
 		//例题:使用lastIndexOf()方法查看字符串str中 空字符串的位置
 		//然后输出字符串长度 注意：lastIndexOf()方法中的参数是空字符串“” 中间没有空格
 		//lastIndexOf()方法中的参数是空字符串“”  则返回结果与调用该字符串length()方法的结果相同
 		String str3="we are students";
 		int size3= str3.lastIndexOf("");
 		System.out.println(size3);//15
+		
+		//charAt()获取指定索引位置的字符
+		String str4="we are students";
+		char mychar=str4.charAt(5);
+		System.out.println("字符为:"+mychar);
+		
+		//获取字符串 substring(int beginIndex, int endIndex)
+		String str5="hello word";
+		String str6=str5.substring(0,3);
+		System.out.println(str6);
+		
+		//去掉空格 trim()
+		String str7 =" java class  ";
+		System.out.println("原来的字符串："+str7);
+		System.out.println("去掉空格之后的字符串："+str7.trim());
+		
 	}
 
 }
